084f9ec0bd574945a4e56888cbc81528a60a2f25
vicky11NJR <160415158+vicky11NJR@users.noreply.github.com>
2024-10-16 15:31:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,14 @@ import pandas as pd
 st.title('🖥️_streamlit_learning')
 st.write('hello all ')
 with st.expander('data'):
-  st.write('rathish')
+  st.write('**rathish**')
   data=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   data
+
+  st.write('X')
+  x= df.drop('species',axis=1)
+  x
+
+  st.write('Y')
+  y = df.species
+  y
