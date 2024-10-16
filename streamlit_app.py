34c30f3dc357fc2fe1e810dddbaf03555f731799
vicky11NJR <160415158+vicky11NJR@users.noreply.github.com>
2024-10-16 20:22:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ data = {'island':island,
         'bill_depth_mm':bill_depth_mm,
         'flipper_length_mm':flipper_length_mm,
         'body_mass_g':body_mass_g,
-        'gender':gender}
+        'sex':gender}
 input_df = pd.DataFrame(data,index=[0])
 input_penguins = pd.concat([input_df,x],axis=0)
 
