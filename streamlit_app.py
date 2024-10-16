d21bbd8c03556f9e00d4747f9b2e1a43a33d8251
vicky11NJR <160415158+vicky11NJR@users.noreply.github.com>
2024-10-16 15:21:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 st.title('🖥️_streamlit_learning')
 st.write('hello all ')
-with st.expand('data'):
+with st.expander('data'):
   st.write('rathish')
   data=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   data
